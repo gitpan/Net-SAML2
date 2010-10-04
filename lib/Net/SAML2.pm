7fea962f988600fba289e6226ad3c86828e902c4
Chris Andrews <chrisa@cpan.org>
2010-10-04 12:38:54 +0000

    Import of CHRISA/Net-SAML2-0.01_01 from CPAN.
    gitpan-cpan-distribution: Net-SAML2
    gitpan-cpan-version:      0.01_01
    gitpan-cpan-path:         CHRISA/Net-SAML2-0.01_01.tar.gz
    gitpan-cpan-author:       CHRISA
    gitpan-cpan-maturity:     developer

---
+++ lib/Net/SAML2.pm
@@ -0,0 +1,106 @@
+package Net::SAML2;
+use strict;
+use warnings;
+
+require 5.008_001;
+
+our $VERSION = '0.01_01';
+$VERSION = eval $VERSION;
+
+=head1 NAME
+
+Net::SAML2 - SAML bindings and protocol implementation
+
+=head1 SYNOPSIS
+
+  # generate a redirect off to the IdP:
+
+        my $idp = Net::SAML2::IdP->new($IDP);
+        my $sso_url = $idp->sso_url('urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect');
+        
+        my $authnreq = Net::SAML2::Protocol::AuthnRequest->new(
+                issuer      => 'http://localhost:3000/metadata.xml',
+                destination => $sso_url,
+        )->as_xml;
+
+        my $redirect = Net::SAML2::Binding::Redirect->new(
+                key => 'sign-nopw-cert.pem',
+                url => $sso_url,
+        );
+
+        my $url = $redirect->sign_request($authnreq);
+
+  # handle the POST back from the IdP, via the browser:
+
+        my $post = Net::SAML2::Binding::POST->new;
+        my $ret = $post->handle_response(
+                $saml_response
+        );
+        
+        if ($ret) {
+                my $assertion = Net::SAML2::Protocol::Assertion->new(
+                        xml => decode_base64($saml_response)
+                );
+
+                # ...
+        }
+
+=head1 DESCRIPTION
+
+Support for the Web Browser SSO profile of SAML2. 
+
+This is a very early release, but one which will correctly perform the
+SSO process.
+
+=head1 MAJOR CAVEATS
+
+=over
+
+=item SP-side protocol only
+
+=item No verification of the signer of received Assertions
+
+=item Limited handling of protocol / network errors
+
+=item Requires XML metadata from the IdP
+
+=back
+
+=cut
+
+# entities
+use Net::SAML2::IdP;
+use Net::SAML2::SP;
+
+# bindings
+use Net::SAML2::Binding::Redirect;
+use Net::SAML2::Binding::Artifact;
+use Net::SAML2::Binding::POST;
+
+# protocol
+use Net::SAML2::Protocol::AuthnRequest;
+use Net::SAML2::Protocol::LogoutRequest;
+use Net::SAML2::Protocol::Assertion;
+
+=pod
+
+=head1 AUTHOR
+
+Chris Andrews <chrisandrews@venda.com>
+
+=head1 COPYRIGHT
+
+The following copyright notice applies to all the files provided in
+this distribution, including binary files, unless explicitly noted
+otherwise.
+
+Copyright 2010 Venda Ltd.
+
+=head1 LICENCE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
